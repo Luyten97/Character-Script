3bf63c276af4c34e5e3fa1de1b87aa606a3bbc52
Luyten <Luyten97@users.noreply.github.com>
2018-03-05 20:25:08 -0600

    Add files via upload

--- lua/autorun/cc_autorun.lua
+++ lua/autorun/cc_autorun.lua
@@ -1,21 +1,21 @@
 if SERVER then
-	AddCSLuaFile("autorun/client/cl_ccinterface.lua")
-	AddCSLuaFile("autorun/client/vgui_elements/CC_Button.lua")
-	AddCSLuaFile("autorun/client/vgui_elements/CC_DModelPanel.lua")
-	AddCSLuaFile("autorun/client/vgui_elements/CC_Arrow.lua")
-	AddCSLuaFile("sh_config.lua")
-	include("sh_config.lua")
-	include("autorun/server/sv_ccmessages.lua")
-	include("autorun/server/sv_ccquery.lua")
-	include("autorun/server/sv_commands.lua")
-	include("autorun/server/sv_pocket.lua")
+	AddCSLuaFile("cscript/client/cl_interface.lua")
+	AddCSLuaFile("cscript/client/vgui_elements/CC_Button.lua")
+	AddCSLuaFile("cscript/client/vgui_elements/CC_DModelPanel.lua")
+	AddCSLuaFile("cscript/client/vgui_elements/CC_Arrow.lua")
+	AddCSLuaFile("cscript/sh_config.lua")
+	include("cscript/sh_config.lua")
+	include("cscript/server/sv_messages.lua")
+	include("cscript/server/sv_query.lua")
+	include("cscript/server/sv_commands.lua")
+	include("cscript/server/sv_pocket.lua")
 		
 end
 
 if CLIENT then
-	include("sh_config.lua")
-	include("autorun/client/cl_ccinterface.lua")
-	include("autorun/client/vgui_elements/CC_Arrow.lua")
-	include("autorun/client/vgui_elements/CC_Button.lua")
-	include("autorun/client/vgui_elements/CC_DModelPanel.lua")
+	include("cscript/sh_config.lua")
+	include("cscript/client/cl_interface.lua")
+	include("cscript/client/vgui_elements/CC_Arrow.lua")
+	include("cscript/client/vgui_elements/CC_Button.lua")
+	include("cscript/client/vgui_elements/CC_DModelPanel.lua")
 end
